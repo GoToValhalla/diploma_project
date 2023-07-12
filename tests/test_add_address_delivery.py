41435aac8442b773e9dd7c2d36a000a9b4b27ceb
Russkikh Konstanin <russ.kostua83@gmail.com>
2023-07-13 00:01:50 +0300

    add test address delivery

--- tests/test_add_address_delivery.py
+++ tests/test_add_address_delivery.py
@@ -2,8 +2,10 @@ import allure
 from selene.support.shared import browser
 
 
-
 def test_add_address_delivery():
     with allure.step('Кликаем на форму указания адреса'):
-    browser.open('https://dostavka.magnit.ru/')
+        browser.open('https://dostavka.magnit.ru/')
     browser.element('[class="select-address__tooltip"]').click()
+
+
+
