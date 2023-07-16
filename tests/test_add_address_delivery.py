9ef352eb8434297d0d4eac3725ccef78dde483c6
Russkikh Konstanin <russ.kostua83@gmail.com>
2023-07-16 21:13:04 +0300

    add test address delivery

--- tests/test_add_address_delivery.py
+++ tests/test_add_address_delivery.py
@@ -8,8 +8,14 @@ def test_add_address_delivery():
     with allure.step('Кликаем на форму указания адреса'):
         browser.open('https://dostavka.magnit.ru/')
     browser.element('[class="select-address__tooltip"]').click()
+    with allure.step('Ставим фокус в форму ввода адреса доставки'):
+        browser.element('[data-test-id="delivery-address-input"]').click()
+
+    # time.sleep(20)
+
     with allure.step('Вводим адрес доставки'):
-        browser.element('[data-test-id="delivery-address-input"]').send_keys('москва тверская 10с1').press_enter()
+        browser.element('[class="m-input-text__input"]').send_keys('москва тверская 10с1')
+        browser.element('[class="m-input-address__suggestions__item"]').click()
     with allure.step('Подтверждаем введенный адрес'):
         browser.element('[data-test-id="delivery-address-select-btn"]').click()
 
