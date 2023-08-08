269f10ec590bfb46d4f7611a346e71f832ea7096
Russkikh Konstanin <russ.kostua83@gmail.com>
2023-08-09 01:03:07 +0300

    add fixture for driver

--- conftest.py
+++ conftest.py
@@ -1,29 +1,10 @@
-import pytest
-from selene import browser
 from selenium import webdriver
-from selenium.webdriver.chrome import options
+from selene import browser
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-def get_option():
-    return webdriver.ChromeOptions()
-
-
-driver = webdriver.Chrome(executable_path='/Users/user/chromedriver-mac-x64/chromedriver',
-                          options=options,
-                          desired_capabilities=get_option().to_capabilities())
-
-
-
-@pytest.fixture()
-def test1():
-    return 555
-
-
-@pytest.fixture(scope='function', autouse=True)
-def browser_management():
-    chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument('--headless')
-    browser.config.driver_options = chrome_options
-    browser.config.window_width = 1400
-    browser.config.window_height = 1200
-    browser.config.base_url = 'https://demoqa.com'
+def browser_config():
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager(version="114.0.5735.90").install()), options=options)
+    browser.config.driver = driver
---
+++ tests/go_to_main_page_test.py
@@ -0,0 +1,5 @@
+from selene.support.shared import browser
+from selene import have, be
+
+def test_open_main_page():
+    browser.open("https://dostavka.magnit.ru/")
