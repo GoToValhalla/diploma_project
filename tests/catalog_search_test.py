ba73ef52b7e81067d09e8bd1849bdf066bcac8ff
Russkikh Konstanin <russ.kostua83@gmail.com>
2023-08-10 23:59:15 +0300

    small improvement

--- tests/catalog_search_test.py
+++ tests/catalog_search_test.py
@@ -1,4 +1,5 @@
 import allure
+import by
 from selene.support.shared import browser
 
 
@@ -6,3 +7,15 @@ def test_catalog_search():
 
     with allure.step('Открываем сайт доставки'):
         browser.open('https://dostavka.magnit.ru/')
+
+    with allure.step('Кликаем на кнопку каталог'):
+        browser.element('[data-test-id="catalog-btn"]').click()
+
+
+    with allure.step(''):
+        $(by.LINK_TEXT('Цена — что надо')).click()
+
+
+
+
+pass
